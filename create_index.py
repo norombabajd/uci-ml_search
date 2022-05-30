06eb5714febe354bff893941f538ae1aa403f8ea
John Daniel <jnoromba@uci.edu>
2022-05-29 19:12:29 -0700

    Update create_index.py

--- create_index.py
+++ create_index.py
@@ -3,15 +3,16 @@ import numpy as np
 import pandas as pd
 import nmslib
 
-data = pd.read_csv('data/datasets.csv')
-data['relevant_info'].fillna("", inplace=True)
-data['relevant_info'] = np.where(data['relevant_info'] == "Provide all relevant information about your data set.", "", data['relevant_info'])
-data['description'] = data['dataset_title']+" "+data['relevant_info']
+data = pd.read_csv('C:\\Users\\johndaniel\\GitHub\\search\\donated_datasets.csv')
+
+data['Abstract'].fillna("", inplace=True)
+data['Abstract'] = np.where(data['Abstract'] == "Provide all relevant information about your data set.", "", data['Abstract'])
+data['description'] = data['Name']+" "+data['Abstract']
 sentences = list(data['description'].values)
 model = SentenceTransformer('sentence-transformers/all-MiniLM-L6-v2')
 embeddings = model.encode(sentences)
 index = nmslib.init(method='hnsw', space='cosinesimil')
 index.addDataPointBatch(embeddings)
 index.createIndex(print_progress=True)
-index.saveIndex("data_index")
+index.saveIndex("dataset_index")
 
